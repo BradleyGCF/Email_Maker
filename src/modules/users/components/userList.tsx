a8a19d8fa9747f4659fa008d694b02416a5f5df4
GermanEH <germanhdev@gmail.com>
2025-01-13 18:09:59 -0300

    Feat. UI - scrollIntoView effect

--- src/modules/users/components/userList.tsx
+++ src/modules/users/components/userList.tsx
@@ -1,6 +1,6 @@
 import { PaginationComponent as Pagination } from '@/modules/core/components/pagination'
 import type { $OrderBy } from '@/modules/core/enum'
-import { Loader } from '@/modules/core/icons'
+// import { Loader } from '@/modules/core/icons'
 import useGetUsers from '@/modules/users/hooks/useGetUsers'
 import { useLocation } from 'react-router-dom'
 import { CardUser } from './card'
@@ -9,18 +9,22 @@ function UserList() {
   const { search } = useLocation()
   const query = Object.fromEntries(new URLSearchParams(search))
 
-  const { data, loading, error } = useGetUsers({
+  const {
+    data,
+    // loading,
+    error,
+  } = useGetUsers({
     q: query.q,
     page: Number(query.page),
     orderBy: query.orderBy as $OrderBy,
   })
 
-  if (loading)
-    return (
-      <div className="w-full flex place-content-center my-8 h-80">
-        <Loader />
-      </div>
-    )
+  // if (loading)
+  //   return (
+  //     <div className="w-full flex place-content-center my-8 h-80">
+  //       <Loader />
+  //     </div>
+  //   )
 
   if (error) return <div>Error: {error}</div>
 
