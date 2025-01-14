74205ac77f85cd2a04486cb236385ecf363c0a02
GermanEH <germanhdev@gmail.com>
2025-01-14 11:31:46 -0300

    Feat. Conditional auth layout for plans view

--- src/layout/Auth.layout.tsx
+++ src/layout/Auth.layout.tsx
@@ -1,3 +1,4 @@
+import Logo from '@/modules/core/ui/logo'
 // import AuthBG from '@/assets/auth_bg.webp'
 // import { Arrow } from '@/modules/core/icons'
 // import { ButtonVariants } from '@/modules/core/ui/button'
@@ -6,31 +7,34 @@
 import {
   // Link,
   Outlet,
+  useLocation,
 } from 'react-router-dom'
-import icon from '/logo_icon.svg'
-import logotype from '/logo_logotype.svg'
 
 export default function AuthLayout() {
   // useAuth()
-
+  const location = useLocation()
   return (
     <div className="min-h-screen mt-20 mb-10">
-      <div className="grid h-full grid-cols-1 px-4">
-        <div className="w-full max-w-md m-auto flex flex-col justify-center items-center gap-10">
-          {/* <div className="w-full mb-4">
+      {location.pathname.includes('/plans') ? (
+        <div className="w-full px-10 flex flex-col justify-center items-center gap-10">
+          <Logo />
+          <Outlet />
+        </div>
+      ) : (
+        <div className="grid h-full grid-cols-1 px-4">
+          <div className="w-full max-w-md m-auto flex flex-col justify-center items-center gap-10">
+            {/* <div className="w-full mb-4">
             <Link className={cn(ButtonVariants(), 'block w-fit')} to={Routes.home}>
               <Arrow direction="left" />
             </Link>
           </div> */}
-          <div className="flex items-center gap-6">
-            <img src={icon} alt="logo" />
-            <img src={logotype} alt="logo" />
-          </div>
-          <div className="w-full p-8 rounded-3xl shadow-auth backdrop-blur-sm">
-            <Outlet />
+            <Logo />
+            <div className="w-full p-8 rounded-3xl shadow-auth backdrop-blur-sm">
+              <Outlet />
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
