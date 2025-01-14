30d550de4d55c1e27750daaba4f07a1a55ba4250
GermanEH <germanhdev@gmail.com>
2025-01-14 18:08:58 -0300

    Refactor. Landing - plan section

---
+++ src/modules/core/components/planContent.tsx
@@ -0,0 +1,41 @@
+import recommended from '/landing/recommended.svg'
+import tick from '/landing/tick.svg'
+import Button from '../ui/button/basicButton'
+import { cn } from '../utils'
+
+interface ContentProps {
+  plan: { id: number; name: string; price: string }
+  advantages?: { id: number; text: string }[]
+}
+
+const planCard = ({ plan, advantages }: ContentProps) => {
+  return (
+    <>
+      <h3 className={cn('text-darkBlue font-black text-[32px]', plan.id === 2 && advantages && 'mt-1')}>Plan {plan.name}</h3>
+      <p>
+        <span className={cn('font-bold', advantages ? 'text-[32px]' : 'text-6')}>{plan.price}$</span>/mes
+      </p>
+      <p className="text-gray text-[15px] font-Avenir">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+        aliqua. Ut enim ad minim veniam
+      </p>
+      {plan.id === 2 && advantages && (
+        <div
+          className="absolute right-0 top-2 py-2"
+          style={{ backgroundImage: `url(${recommended})`, backgroundRepeat: 'no-repeat' }}
+        >
+          <p className="text-white uppercase text-sm ml-2 px-2 font-bold font-inter self-center mt-1">Recomendado</p>
+        </div>
+      )}
+      {advantages?.map((a) => (
+        <div key={a.id} className="flex gap-2 mx-6 text-gray text-[15px] font-Avenir">
+          <img src={tick} alt="tick" />
+          {a.text}
+        </div>
+      ))}
+      <Button text={advantages ? 'Comprar ahora' : 'Cambiar plan'} className="w-full" />
+    </>
+  )
+}
+
+export default planCard
