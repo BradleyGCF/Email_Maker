caec05da4b64957cef7fba8b895069a7f7ade0a7
GermanEH <germanhdev@gmail.com>
2025-01-20 19:11:28 -0300

    Feat. chatbot bubble chat side icon

--- src/modules/chatbot/components/chatBubble.tsx
+++ src/modules/chatbot/components/chatBubble.tsx
@@ -2,13 +2,18 @@ import ChatWindow from './chatWindow'
 // import { UseBubbleChat } from '../hooks/useBubbleChat'
 
 const ChatBubble = () => {
-  // const { ref, handleOpenBox, bubbleBox } = UseBubbleChat()
+  // const {
+  //   // ref, handleOpenBox,
+  //   // bubbleBox
+  // } = UseBubbleChat()
 
   return (
     <div
     // ref={ref}
     >
-      <ChatWindow />
+      <ChatWindow
+      // open={bubbleBox}
+      />
     </div>
   )
 }
--- src/modules/chatbot/components/chatWindow.tsx
+++ src/modules/chatbot/components/chatWindow.tsx
@@ -4,6 +4,7 @@ import { useUserStore } from '@/store/user'
 import { PaperAirplaneIcon } from '@heroicons/react/24/solid'
 import { useState } from 'react'
 import { toast } from 'sonner'
+import chatBubbleSideIcon from '/chatbot/index.svg'
 
 // type Props = {
 //   open: boolean
@@ -55,52 +56,55 @@ const ChatWindow = (
         'scale-1 opacity-100 translate-y-0 translate-x-0'
       )}
     >
-      <div
-        className={cn(
-          'flex flex-col gap-3 bg-white shadow-lg rounded-lg z-50 w-[300px] md:w-[400px] lg:w-[600px] transform transition-all duration-150 bg-white border'
-        )}
-      >
-        <div className="flex flex-col mb-4 text-black">
-          <div className="flex flex-col gap-8 min-h-20">
-            {messages?.map((message: Message, index) => {
-              return (
-                <div key={message.timestamp}>
-                  <div
-                    key={message.timestamp}
-                    className="rounded-[10px] bg-secondary hover:bg-orange-600 active:bg-orange-600 p-2"
-                  >
-                    <p>{message.text}</p>
+      <div className="relative p-4">
+        <img src={chatBubbleSideIcon} alt="chatBubbleSideIcon" className="absolute top-20 left-0" />
+        <div
+          className={cn(
+            'flex flex-col gap-3 bg-white shadow-chatbot rounded-lg z-50 w-[300px] md:w-[400px] lg:w-[600px] transform transition-all duration-150 bg-white'
+          )}
+        >
+          <div className="flex flex-col mb-4 text-black">
+            <div className="flex flex-col gap-8 min-h-20">
+              {messages?.map((message: Message, index) => {
+                return (
+                  <div key={message.timestamp}>
+                    <div
+                      key={message.timestamp}
+                      className="rounded-[10px] bg-secondary hover:bg-orange-600 active:bg-orange-600 p-2"
+                    >
+                      <p>{message.text}</p>
+                    </div>
+                    <p className={cn('flex', index % 2 === 0 && 'justify-end')}>{message.timestamp}</p>
                   </div>
-                  <p className={cn('flex', index % 2 === 0 && 'justify-end')}>{message.timestamp}</p>
-                </div>
-              )
-            })}
+                )
+              })}
+            </div>
           </div>
-        </div>
-        <div className="flex p-6 bg-extraLightBlue rounded-b-lg">
-          <input
-            type="text"
-            className="flex-grow h-full h-6 border border-gray-300 rounded-lg bg-white focus:border-secondary focus:ring-0"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                handleSendMessage()
-              }
-            }}
-          />
-          <button
-            className={`flex items-center h-full ${loading ? 'bg-gray-400' : ''} text-white rounded-r-lg`}
-            onClick={handleSendMessage}
-            type="button"
-            disabled={loading}
-          >
-            {loading ? (
-              <div className="loading loading-spinner w-6" />
-            ) : (
-              <PaperAirplaneIcon className="w-6" style={{ transform: 'rotate(-35deg)' }} fill="#5C8CC0" />
-            )}
-          </button>
+          <div className="flex p-6 bg-extraLightBlue rounded-b-lg">
+            <input
+              type="text"
+              className="flex-grow h-full h-6 border border-gray-300 rounded-lg bg-white focus:border-secondary focus:ring-0"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  handleSendMessage()
+                }
+              }}
+            />
+            <button
+              className={`flex items-center h-full ${loading ? 'bg-gray-400' : ''} text-white rounded-r-lg`}
+              onClick={handleSendMessage}
+              type="button"
+              disabled={loading}
+            >
+              {loading ? (
+                <div className="loading loading-spinner w-6" />
+              ) : (
+                <PaperAirplaneIcon className="w-6" style={{ transform: 'rotate(-35deg)' }} fill="#5C8CC0" />
+              )}
+            </button>
+          </div>
         </div>
       </div>
     </div>
